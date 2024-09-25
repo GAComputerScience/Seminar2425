a6349b1e61eac2cf9bb96e72814e65aaef0f2609
ellebrew <ebrew@greenwichacademy.org>
2024-09-24 22:10:15 -0400

    EBList.java

---
+++ EBList.java
@@ -0,0 +1,44 @@
+public class EBList{
+    private String[] array;
+    //number of values (logical size)
+    private int size;
+
+    public EBList(){
+        array = new String[10];
+        size = 0;
+    }
+
+    public boolean add(String s){
+
+    }
+
+    public boolean add (int index, String s){
+
+    }
+
+    public String get(int index){
+
+    }
+
+    public String remove (int index){
+
+    }
+
+    public int indexOf(String s){
+
+    }
+
+    public int size(){
+
+    }
+
+    private String[] resize(String[] x){
+
+    }
+
+    public boolean contains(String x){
+
+    }
+
+
+}
